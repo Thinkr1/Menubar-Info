26a55d22786f9037e83cfb20c0d04f03649ec8db
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-11-02 15:57:31 +0100

    Added settings window with slider to modify pct refresh rate

--- CPU-Menubar/CPU_MenubarApp.swift
+++ CPU-Menubar/CPU_MenubarApp.swift
@@ -6,11 +6,16 @@
 //
 
 import SwiftUI
+import Combine
 
 @main
 struct CPU_MenubarApp: App {
-    @State private var cpuUsage: String = "0%"
-    private let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    @Environment(\.openWindow) private var openWindow
+    @State private var cpuUsage: String = "0%" // default cpu usage value
+    @State private var refreshRate: TimeInterval = 5 // default
+    private var timer: Publishers.Autoconnect<Timer.TimerPublisher> { // using Combine to deliver elements to subscribers (get refresh rate)
+        Timer.publish(every: refreshRate, on:.main, in: .common).autoconnect() // refresh rate
+    }
     var body: some Scene {
         MenuBarExtra() {
             VStack {
@@ -23,6 +28,9 @@ struct CPU_MenubarApp: App {
                     updateCPUUsage()
                 }.keyboardShortcut("r")
                 Divider()
+                Button("Settings") {
+                    openWindow(id:"settings")
+                }
                 Button("Quit") {
                     NSApplication.shared.terminate(nil)
                 }.keyboardShortcut("q")
@@ -32,12 +40,16 @@ struct CPU_MenubarApp: App {
             Image(systemName: "gauge")
             .labelStyle(.titleAndIcon)
         }
+
+        WindowGroup("Settings", id: "settings") { // settings window
+            SettingsView(refreshRate: $refreshRate, isEditingRefreshRate: $isEditingRefreshRate)
+        }
     }
     private func updateCPUUsage() {
         let process = Process()
         let pipe = Pipe()
         process.executableURL = URL(fileURLWithPath: "/bin/zsh")
-        process.arguments = ["-c", "ps -A -o %cpu | awk '{s+=$1} END {print s \"%\"}'"]
+        process.arguments = ["-c", "ps -A -o %cpu | awk '{s+=$1} END {print s \"%\"}'"] // using ps command to get cpu %, rendering with awk
         process.standardOutput = pipe
         
         do {
@@ -52,8 +64,26 @@ struct CPU_MenubarApp: App {
         
         if let output = String(data: data, encoding: .utf8) {
             DispatchQueue.main.async {
-                cpuUsage = output.trimmingCharacters(in: .whitespacesAndNewlines)
+                cpuUsage = output.trimmingCharacters(in: .whitespacesAndNewlines) // render output correctly
             }
+        }
+    }
+}
+
+struct SettingsView: View {
+    // bind vars to app struct vars
+    @Binding var refreshRate: TimeInterval
+
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.headline)
+                .padding()
+            
+            Slider(value: $refreshRate, in: 1...60, step: 1) {
+                Text("Refresh Rate: \(refreshRate, specifier: "%.2f") seconds")
+            }.padding()
+            
         }
     }
 }
