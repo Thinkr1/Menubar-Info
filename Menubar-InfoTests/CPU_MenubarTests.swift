3657bbde101ac7452df7d723a946814a6601e3f1
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-11-28 20:21:29 +0100

    Changed project name

---
+++ Menubar-InfoTests/CPU_MenubarTests.swift
@@ -0,0 +1,17 @@
+//
+//  CPU_MenubarTests.swift
+//  CPU-MenubarTests
+//
+//  Created by [REDACTED] on 03/11/2024.
+//
+
+import Testing
+@testable import CPU_Menubar
+
+struct CPU_MenubarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
