c91d18511cd87ecfe46a2791b593dfcbdef63f7d
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2025-05-17 20:24:37 +0200

    feat: added tab view with simulation and articles
    more improvements too

--- Menubar-Info/Menubar_InfoApp.swift
+++ Menubar-Info/Menubar_InfoApp.swift
@@ -22,3 +22,43 @@ struct Menubar_InfoApp: App {
         }
     }
 }
+//
+//struct CustomMenuItem: Identifiable, Codable {
+//    let id: UUID
+//    var title: String
+//    var command: String
+//    var refreshInterval: TimeInterval?
+//    var outputFormat: String?
+//    var showInMenuBar: Bool
+//    
+//    init(id: UUID = UUID(),
+//         title: String,
+//         command: String,
+//         refreshInterval: TimeInterval? = nil,
+//         outputFormat: String? = nil,
+//         showInMenuBar: Bool = false) {
+//        self.id = id
+//        self.title = title
+//        self.command = command
+//        self.refreshInterval = refreshInterval
+//        self.outputFormat = outputFormat
+//        self.showInMenuBar = showInMenuBar
+//    }
+//}
+//
+//struct CustomMenuButton: Identifiable, Codable {
+//    let id: UUID
+//    var title: String
+//    var items: [CustomMenuItem]
+//    var isVisible: Bool
+//    
+//    init(id: UUID = UUID(),
+//         title: String,
+//         items: [CustomMenuItem] = [],
+//         isVisible: Bool = true) {
+//        self.id = id
+//        self.title = title
+//        self.items = items
+//        self.isVisible = isVisible
+//    }
+//}
